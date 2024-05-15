5f3bd8db131c7d6d06e5c503b8fa9e1730365a18
ajaydhakane <118625131+ajaydhakane@users.noreply.github.com>
2024-05-15 14:44:19 +0530

    Delete lab04 directory

--- lab04/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./dis/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-//npx tailwindcss -i ./sorce/input.css -o ./dis/style.css --watch
